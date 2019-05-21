6e32380d2a14cdef8f21acd7d6f1a4084bb41254
MarianaPatrii <maryana.patrii@gmail.com>
2019-05-21 08:47:50 +0300

    react_poke

---
+++ src/components/layout/Loading.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactLoading from 'react-loading';
+export default class Loading extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h5>Getting Pokemon items...</h5>
+      </React.Fragment>
+    );
+  }
+}
---
+++ src/components/pokemon/Pokemon.js
@@ -0,0 +1,183 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+
+const TYPE_COLORS = {
+  bug: 'B1C12E',
+  dark: '4F3A2D',
+  dragon: '755EDF',
+  electric: 'FCBC17',
+  fairy: 'F4B1F4',
+  fighting: '823551D',
+  fire: 'E73B0C',
+  flying: 'A3B3F7',
+  ghost: '6060B2',
+  grass: '74C236',
+  ground: 'D3B357',
+  ice: 'A3E7FD',
+  normal: 'C8C4BC',
+  poison: '934594',
+  psychic: 'ED4882',
+  rock: 'B9A156',
+  steel: 'B5B5C3',
+  water: '3295F6'
+};
+
+export default class Pokemon extends Component {
+  state = {
+    name: '',
+    pokemonIndex: '',
+    imageUrl: '',
+    types: [],
+    description: '',
+    statTitleWidth: 3,
+    statBarWidth: 9,
+    stats: {
+      hp: '',
+      attack: '',
+      defense: '',
+      speed: '',
+      specialAttack: '',
+      specialDefense: ''
+    },
+  };
+
+  async componentDidMount() {
+    const { pokemonIndex } = this.props.match.params;
+
+    // Urls for pokemon information
+    const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`;
+
+    // Get Pokemon Information
+    const pokemonRes = await Axios.get(pokemonUrl);
+
+    const name = pokemonRes.data.name;
+    const imageUrl = pokemonRes.data.sprites.front_default;
+
+    let { hp, attack, defense, speed, specialAttack, specialDefense } = '';
+
+    pokemonRes.data.stats.map(stat => {
+      switch (stat.stat.name) {
+        case 'hp':
+          hp = stat['base_stat'];
+          break;
+        case 'attack':
+          attack = stat['base_stat'];
+          break;
+        case 'defense':
+          defense = stat['base_stat'];
+          break;
+        case 'speed':
+          speed = stat['base_stat'];
+          break;
+        case 'special-attack':
+          specialAttack = stat['base_stat'];
+          break;
+        case 'special-defense':
+          specialDefense = stat['base_stat'];
+          break;
+        default:
+          break;
+      }
+    });
+
+    const types = pokemonRes.data.types.map(type => type.type.name);
+
+       const abilities = pokemonRes.data.abilities
+      .map(ability => {
+        return ability.ability.name
+          .toLowerCase()
+          .split('-')
+          .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+          .join(' ');
+      })
+      .join(', ');
+
+
+    this.setState({
+      imageUrl,
+      pokemonIndex,
+      name,
+      types,
+      stats: {
+        hp,
+        attack,
+        defense,
+        speed,
+        specialAttack,
+        specialDefense
+      },
+
+    });
+  }
+
+  render() {
+    return (
+      <div className="col">
+        <div className="card">
+          <div className="card-body">
+            <div className="row align-items-center">
+              <div className=" col-md-3 ">
+                <img
+                  src={this.state.imageUrl}
+                  className="card-img-top rounded mx-auto mt-2"
+                />
+                <div className="col-md-12">
+                <h4 className="mx-auto">
+                  {this.state.name
+                    .toLowerCase()
+                    .split(' ')
+                    .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+                    .join(' ')}  #{this.state.pokemonIndex}
+                </h4>
+                {this.state.types.map(type => (
+                    <span
+                      key={type}
+                      className="badge badge-pill mr-1"
+                      style={{
+                        backgroundColor: `#${TYPE_COLORS[type]}`,
+                        color: 'white'
+                      }}
+                    >
+                      {type
+                        .toLowerCase()
+                        .split(' ')
+                        .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+                        .join(' ')}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="col-md-12">
+                 <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    HP
+                  </div>{this.state.stats.hp}</div>
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    Attack
+                  </div> {this.state.stats.attack} </div>
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    Defense
+                  </div>{this.state.stats.defense}</div>
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    Speed
+                  </div>{this.state.stats.speed}</div>
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    Sp Atk
+                  </div>{this.state.stats.specialAttack}</div>
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    Sp Def
+                  </div>{this.state.stats.specialDefense}</div>
+                </div>
+              </div>
+            </div> 
+          </div>
+         </div>
+        
+    );
+  }
+}
